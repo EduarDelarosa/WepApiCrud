de5c32c62f0ac13cc4d9cc4ba489d0147d64002d
Eduard <eduardelarosa09@gmail.com>
2022-04-10 08:50:52 -0500

    primer commit

---
+++ BackEnd/Controllers/TarjetaCreditoController.cs
@@ -0,0 +1,105 @@
+﻿using BackEnd.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarjetaCreditoController : ControllerBase
+    {
+        private readonly AplicationDbContext _context;
+
+        public TarjetaCreditoController(AplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //GET: api/TarjetaCredito
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TarjetaCredito>>> GetTarjetaCredito()
+        {
+            return await _context.TarjetaCredito.ToListAsync();
+        }
+
+        //Get: api/TarjetaCredito/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TarjetaCredito>> GetTarjetaCredito(int id)
+        {
+            var tarjetaCredito = await _context.TarjetaCredito.FindAsync(id);
+
+            if (tarjetaCredito == null)
+            {
+                return NotFound();
+            }
+
+            return tarjetaCredito;
+        }
+
+        //PUT: api/TarjetaCredito
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTarjetaCredito(int id, TarjetaCredito tarjetaCredito)
+        {
+            if (id != tarjetaCredito.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tarjetaCredito).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (System.Exception)
+            {
+                if (!TarjetaCreditoExist(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        //POST: api/TarjetaCredito
+        [HttpPost]
+        public async Task<ActionResult<TarjetaCredito>> PostTarjetaCredito(TarjetaCredito tarjetaCredito)
+        {
+            _context.TarjetaCredito.Add(tarjetaCredito);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTarjetaCredito", new { id = tarjetaCredito.Id }, tarjetaCredito);
+        }
+
+        //DELETE: api/TarjetaCredito
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<TarjetaCredito>> DeleteTarjetaCredito(int id)
+        {
+            var tarjetaCredito = await _context.TarjetaCredito.FindAsync(id);
+            if (tarjetaCredito == null)
+            {
+                return NotFound();  
+            }
+
+            _context.TarjetaCredito.Remove(tarjetaCredito);
+            await _context.SaveChangesAsync();
+
+            return tarjetaCredito;
+        }
+
+        private bool TarjetaCreditoExist(int id)
+        {
+            return _context.TarjetaCredito.Any(x => x.Id == id);
+        }
+    }
+}
---
+++ BackEnd/obj/Release/netcoreapp3.1/PubTmp/EFSQLScripts/BackEnd.AplicationDbContext.sql
@@ -0,0 +1,33 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220213033922_Inicial')
+BEGIN
+    CREATE TABLE [TarjetaCredito] (
+        [Id] int NOT NULL IDENTITY,
+        [NombreTitular] varchar(100) NOT NULL,
+        [NumeroTarjeta] varchar(16) NOT NULL,
+        [FechaExpiracion] varchar(5) NOT NULL,
+        [Cvv] varchar(3) NOT NULL,
+        CONSTRAINT [PK_TarjetaCredito] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220213033922_Inicial')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20220213033922_Inicial', N'3.1.4');
+END;
+
+GO
+
